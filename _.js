6868bd37e4bc430c5a3617ea170c1f49ed1592c8
Kris Norful <krisnorful@gmail.com>
2021-06-19 22:03:35 -0500

    implemented pad

--- _.js
+++ _.js
@@ -22,6 +22,26 @@ const _ = {
   words(str) {
     return str.split(" ");
   },
+  pad(str, length) {
+    //if length parameter is less than str length, return str
+    if (length < str.length) return str;
+    //subract length of string from length parameter to see how much space is needed
+    let paddingNeeded = length - str.length;
+    //if even amount of space, split evenly before and after string
+    if (paddingNeeded % 2 === 0) {
+      return (
+        " ".repeat(paddingNeeded / 2) + str + " ".repeat(paddingNeeded / 2)
+      );
+    } else {
+      //if odd, subtract one from space needed and add to end of string
+      return (
+        " ".repeat(Math.floor(paddingNeeded / 2)) +
+        str +
+        " ".repeat(Math.floor(paddingNeeded / 2)) +
+        " "
+      );
+    }
+  },
 };
 
 // Do not write or modify code below this line.
