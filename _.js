10860897ff26939daf925ca998dcc9da29f2d661
Kris Norful <krisnorful@gmail.com>
2021-06-19 21:48:48 -0500

    implement inRange

--- _.js
+++ _.js
@@ -4,6 +4,21 @@ const _ = {
     else if (num > upper) return upper;
     else return num;
   },
+  inRange(num, start, end) {
+    if (!end) {
+      end = start;
+      start = 0;
+    }
+
+    if (start > end) {
+      temp = start;
+      start = end;
+      end = temp;
+    }
+    if (num < start) return false;
+    else if (num >= end) return false;
+    else return true;
+  },
 };
 
 // Do not write or modify code below this line.
